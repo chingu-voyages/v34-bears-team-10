4aa659fc8a81ea6473fe3a1b6596e7d206bbf023
Kerosz <andrei_chirila@outlook.com>
2021-10-11 11:51:02 +0300

    chore: config files clean up

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx,json,md,yaml,yml,graphql}": ["pnpm format"],
-  "*.{js,jsx,ts,tsx}": ["pnpm lint"],
+  "*.{js,jsx,json,md,yaml,yml,graphql}": ["pnpm format"],
+  "*.{js,jsx}": ["pnpm lint"],
 };
