b6f9672e90a9cd1e16dcfcac5c717ad7fb9c57d7
Kerosz <andrei_chirila@outlook.com>
2021-10-15 17:54:03 +0300

    chore: add theme schemas

---
+++ src/theme/schemas/dark.js
@@ -0,0 +1,19 @@
+/**
+ * Dark 'palette' is the same as the `light` one for now
+ * we might want to change this if we add theming at
+ * a later point.
+ */
+export const darkSchema = {
+  primary: {
+    light: "#ffffff",
+    main: "#ffffff",
+    dark: "#cccccc",
+  },
+  secondary: {
+    light: "#5c5c5c",
+    main: "#333333",
+    dark: "#0c0c0c",
+  },
+};
+
+export default darkSchema;
